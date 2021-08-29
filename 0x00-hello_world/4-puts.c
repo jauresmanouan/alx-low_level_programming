82a4314715e2989fcedf02b45496d7683f46d9d4
Jaures Manouan <jauresmanouan@gmail.com>
2021-08-29 09:37:24 +0000

    Use puts to display text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print message with puts
+ *
+ * Return: 0 if everything is nice
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
