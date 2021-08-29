2a16e50dd4f1561eb06328975530c9506a05dcc4
Jaures Manouan <jauresmanouan@gmail.com>
2021-08-29 09:46:40 +0000

    Use printf to display content

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - use printf to display message
+ *
+ * Return: 0 if everything is nice
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
