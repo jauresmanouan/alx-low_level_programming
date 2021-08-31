1294098e6d40900cb88a88626fdd99a8b429f96a
Jaures Manouan <jauresmanouan@gmail.com>
2021-08-31 02:14:23 +0000

    Modulo of an integer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
-printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+printf("Last digit of %i is %i and is greater than 5 and not 0\n", n, n % 10);
 else
 if (n % 10 < 6)
-printf("Last digit of %i is %i and is less than 6\n", n, n % 10);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 else
-printf("Last digit of %i is %i and is zero\n", n, n % 10);
+printf("Last digit of %i is %i and is zero and is 0\n", n, n % 10);
 return (0);
 }
