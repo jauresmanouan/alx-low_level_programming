f9fa140f616aae7f417b0c946ca2096619f16fa2
Jaures Manouan <jauresmanouan@gmail.com>
2021-08-31 01:17:47 +0000

    Positive, negative or null number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+printf("%i is positif \n", n);
+else
+if (n < 0)
+printf("%i is negatif \n", n);
+else
+printf("%i is zero \n", n);
+return (0);
+}
