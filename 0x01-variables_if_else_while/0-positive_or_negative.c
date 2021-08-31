7d1baca349b12c5a5f06c9ece579d54707f6bb36
Jaures Manouan <jauresmanouan@gmail.com>
2021-08-31 01:26:32 +0000

    Positive, negative or null number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("%i is positif\n", n);
+printf("%i is positif \n", n);
 else
 if (n < 0)
-printf("%i is negatif\n", n);
+printf("%i is negatif \n", n);
 else
 printf("%i is zero\n", n);
 return (0);
