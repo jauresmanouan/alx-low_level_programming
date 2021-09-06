977a90fd9b4ec4d7bc11798aa53297beb65a3d4e
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-06 18:31:50 +0000

    Number with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * main - numberz
+ *
+ * Return: 0 if s true
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
