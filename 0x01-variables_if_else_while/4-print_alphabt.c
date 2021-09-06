2d0ecef0cac952d20ebc694f3bd066026736ea4a
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-06 17:48:52 +0000

    Alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+/**
+ * main - lower case without e and q
+ *
+ * Return: 0 if all is good
+ *
+ */
+
+int main(void)
+{
+char l;
+l = 'a';
+while (l <= 'z')
+{
+if (l != 'e' && l != 'q')
+{
+putchar(l);
+}
+l++;
+}
+putchar(10);
+}
