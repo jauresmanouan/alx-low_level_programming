fd27cf3d7049bc64963d26612d1db2e36303d634
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-06 18:18:59 +0000

    Number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <math.h>
+/**
+ * main - number
+ *
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+putchar(10);
+return (0);
+}
