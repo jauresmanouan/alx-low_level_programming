045cf93ee4e66ef3bf038d96de14ae9a8d15d18d
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-07 06:41:05 +0000

    Number and Alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+/**
+ * main - number and alphabet
+ *
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+for (i = 97; i <= 122; i++)
+{
+putchar (i);
+}
+putchar(10);
+}
