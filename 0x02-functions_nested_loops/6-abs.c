26d861bd16ee45bea7d9845ae4c5b8900e43b115
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-09 23:09:03 +0000

    Return the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * _abs - absolute volue
+ * @i: receive value
+ * Return: the absolute value
+ */
+
+int _abs(int i)
+{
+if (i < 0)
+return (-i);
+else
+return (i);
+return (0);
+}
