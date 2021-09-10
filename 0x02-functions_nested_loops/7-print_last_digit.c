e5f6e235420d5b8438915e73150d5e26e572a19a
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-10 03:16:10 +0000

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,6 +5,7 @@
  * @i: value
  * Return: the last digit
  */
+
 int print_last_digit(int i)
 {
 int a;
@@ -13,6 +14,6 @@ a = i % 10;
 if (a < 0)
 a *= -1;
 _putchar(a + 48);
- return (0);
+return (a);
 
 }
