03cfdb6265fe8de6edfed84cb3003092ee861229
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-14 02:44:22 +0000

    Debugging task 2 : compare 3 numers among them_add more condition

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,14 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if ((a > b && b > c) || (a > b && a > c))
 {
 largest = a;
 }
 
-else if (b > a && a > c)
+else if ((b > a && a > c) || (b > a && b > c))
 {
 largest = b;
-}
-
-else if (a > b && c > b && a > c)
-{
-largest = a;
 }
 
 else
