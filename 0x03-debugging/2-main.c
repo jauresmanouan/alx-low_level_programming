735309204bd9b299e654c3f2d349197e4dd88de0
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-14 23:32:49 +0000

    Compare 3 numbers among them

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = -972;
-        b = -98;
-        c = 10;
+        a = 0;
+        b = -972;
+        c = -972;
 
         largest = largest_number(a, b, c);
 
