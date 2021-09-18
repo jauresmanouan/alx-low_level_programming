b16465824c96ef090eeb0659c7bf5381091503c9
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-18 10:47:21 +0000

    mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiple 2 number and give the result
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
