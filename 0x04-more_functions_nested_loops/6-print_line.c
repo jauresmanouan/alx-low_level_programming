bceaaa23a5b1fc85b80b763c3b2d499cf8b3ea26
Jaures Manouan <jauresmanouan@gmail.com>
2021-09-18 16:29:47 +0000

    print_line function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - just to put dash
+ * @n: number of times
+ */
+
+void print_line(int n)
+{
+int i = 0;
+
+for (; i < n; )
+{
+_putchar(95);
+i++;
+}
+_putchar(10);
+}
