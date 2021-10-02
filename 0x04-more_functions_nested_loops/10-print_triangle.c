7a4d5a824d8d38cb9166aea23965e5d74978efdb
Jaures Manouan <jauresmanouan@gmail.com>
2021-10-02 02:15:11 +0000

    print_triangle - functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,41 @@
 #include "main.h"
 
-/***/
+/**
+ * print_triangle - display triangle
+ *@size: give the size
+ */
 
 void print_triangle(int size)
 {
-  int i = 1;
-  
-  if (size < 0)
-    {
-      _putchar(10);
-    }
-  if (size > 0)
-    {
-      while (i <= size)
-	{
-	  int x = 1;
-	  
-	  for(; x<=size; x++)
-	    {
-	      _putchar(32);
-	    }
-	  _putchar('#');
-	  _putchar(10);
-	  i++;
-	}
-    }
+int i = 0, x = size;
+if (size <= 0)
+{
+_putchar(10);
+return;
+}
+int y = 0, j = 0;
+for (; x >= i;)
+{
+if (x > 1 && x != 1)
+_putchar(32);
+i++;
+if ((i + 1) == x)
+{
+j = size - i;
+y = 0;
+for (; y < j; y++)
+_putchar(35);
+i = 0;
+x = size - y;
+_putchar(10);
+}
+
+if (x == 1)
+{
+for (i = 0; i < size; i++)
+_putchar(35);
+_putchar(10);
+return;
+}
+}
 }
---
+++ 0x05-pointers_arrays_strings/.#5-rev_string.c
@@ -0,0 +1,1 @@
+jaures@jaures-Lenovo-IdeaPad-S540-13IML.217317:1633133013
