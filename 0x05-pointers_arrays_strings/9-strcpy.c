2274b0e87686f7a6f370beb97943b3f8f0942743
Jaures Manouan <jauresmanouan@gmail.com>
2021-10-06 18:05:44 +0000

    strcy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string content
+ * @dest: destination
+ * @src: source
+ * Return: the value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+
+return (start);
+}
