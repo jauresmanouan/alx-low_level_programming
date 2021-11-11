ca82e423b42db2e053568277985b7e224e230585
Jaures Manouan <jauresmanouan@gmail.com>
2021-11-11 13:27:13 +0000

    strcmp functioncorrection

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
-/***/
+/**
+ *_strcmp- count char
+ *@s1: first char
+ *@s2: second char
+ *Return: difference between both unsigned char
+ */
 
 int _strcmp(char *s1, char *s2)
 {
-  while (*s1)
-    {
-      if (*s1 != *s2)
-	break;
-      s1++;
-      s2++;
-    }
-  return (*(unsigned char*)s1 - *(unsigned char*)s2);
+while (*s1)
+{
+if (*s1 != *s2)
+break;
+s1++;
+s2++;
+}
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
