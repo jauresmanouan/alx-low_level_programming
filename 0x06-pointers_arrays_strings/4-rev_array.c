03bddab359e5a41f0317f256423fd8d7d9c3ed81
Jaures Manouan <jauresmanouan@gmail.com>
2021-12-08 01:42:31 +0000

    reverse_function

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - functions to reverse array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	for (i; i < n / 2; i++, n--)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
