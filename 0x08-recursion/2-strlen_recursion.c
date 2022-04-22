982d1a768870bdfd5b80b71c80388d011f0fe3ba
Jaures Manouan <jauresmanouan@gmail.com>
2022-04-22 11:15:23 +0000

    fix : _strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,6 +8,16 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
+	int c;
+
+	c = 0;
+
+	if (*s == '\0')
+		return (0);
+
+	else
+	{
+		c = 1 + _strlen_recursion(s + 1);
+		return (c);
+	}
 }
